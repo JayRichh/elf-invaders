f53f2ccaeceab4248f7a688ba2896040e4e98ea3
Jayden <jsrich@hotmail.co.nz>
2022-12-05 17:54:34 +0000

    heroku test

--- server.js
+++ server.js
@@ -13,18 +13,6 @@ const serveStatic = require("serve-static");
 const path = require("path");
 require("dotenv").config();
 
-app.use(cors()); // to allow cross origin requests
-app.use(bodyParser.json()); // to convert the request into JSON
-
-//here we are configuring dist to serve app files in client/dist folder
-app.use("/", serveStatic(path.join(__dirname, "client/dist")));
-
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
-});
-
-
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -33,9 +21,17 @@ mongoose
   .then(() => console.log(`MongoDB database Connected... ${MONGO_URI}`))
   .catch((err) => console.log(err));
 
+app.use(cors()); // to allow cross origin requests
+app.use(bodyParser.json()); // to convert the request into JSON
+
 app.use("/api/", Player);
 app.use("/api/", Elf);
 app.use("/api/", HighScore);
+
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
