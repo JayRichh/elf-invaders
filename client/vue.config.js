b21918fee94332228030d9d7fa253b841d25dd84
Jayden <jsrich@hotmail.co.nz>
2022-12-05 20:34:47 +0000

    update api uri

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,13 +2,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-        },
-      },
-    },
-  },
 })
